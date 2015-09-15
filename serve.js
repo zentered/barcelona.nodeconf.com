2b61c71451d0362aba2cb22c7cc11b2641e69149
Patrick Heneise <patrickheneise@fastmail.com>
2015-09-15 13:28:43 +0200

    new partial handling

--- serve.js
+++ serve.js
@@ -1,14 +1,11 @@
 var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
-var templates = require('metalsmith-templates');
+var layouts = require('metalsmith-layouts');
 var collections = require('metalsmith-collections');
 var assets = require('metalsmith-assets');
 var permalinks = require('metalsmith-permalinks');
 var browserSync = require('metalsmith-browser-sync');
 var sass = require('metalsmith-sass');
-var setuphandlebars = require('./setuphandlebars');
-
-setuphandlebars.init();
 
 Metalsmith(__dirname)
   .source('src/')
@@ -22,10 +19,13 @@ Metalsmith(__dirname)
     pattern: ':collection/:title'
   }))
   .use(markdown())
-  .use(templates('handlebars'))
+  .use(layouts({
+    engine: 'handlebars',
+    partials: 'partials'
+  }))
   .use(sass({
     outputStyle: 'expanded',
-    outputDir: 'css/'
+    outputDir: 'assets/css/'
   }))
   .use(assets({
     source: './assets', // relative to the working directory
