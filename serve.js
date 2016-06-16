93878f3b6125ca25b57602b6881cd2f17fc16199
Patrick <patrick@blended.io>
2016-06-16 08:22:52 +0200

    Archive 2015

--- serve.js
+++ serve.js
@@ -1,11 +1,11 @@
-var Metalsmith = require('metalsmith');
-var markdown = require('metalsmith-markdown');
-var layouts = require('metalsmith-layouts');
-var assets = require('metalsmith-assets');
-var permalinks = require('metalsmith-permalinks');
-var browserSync = require('metalsmith-browser-sync');
-var metadata = require('metalsmith-metadata');
-var sass = require('metalsmith-sass');
+var Metalsmith = require('metalsmith')
+var markdown = require('metalsmith-markdown')
+var layouts = require('metalsmith-layouts')
+var assets = require('metalsmith-assets')
+var permalinks = require('metalsmith-permalinks')
+var browserSync = require('metalsmith-browser-sync')
+var metadata = require('metalsmith-metadata')
+var sass = require('metalsmith-sass')
 
 Metalsmith(__dirname)
   .source('src/')
@@ -27,15 +27,19 @@ Metalsmith(__dirname)
     outputDir: 'assets/css/'
   }))
   .use(assets({
+    source: './2015', // relative to the working directory
+    destination: './2015' // relative to the build directory
+  }))
+  .use(assets({
     source: './assets', // relative to the working directory
     destination: './assets' // relative to the build directory
   }))
   .use(browserSync({
     server: './build',
-    files: ['src/**/*.*','layouts/*.html','partials/*.html']
+    files: ['src/**/*.*', 'layouts/*.html', 'partials/*.html']
   }))
-  .build(function(error) {
+  .build(function (error) {
     if (error) {
-      console.log(error);
+      console.log(error)
     }
-  });
+  })
